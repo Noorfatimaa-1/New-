af667d8304802e0f27bcd71b120926ae5346d7fb
Noorfatimaa-1 <hussainali2574@gmail.com>
2024-09-22 06:06:30 -0700

    Add files via upload

---
+++ code11.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ system("Color 70");
+ cout<<"    ###     "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  #######   "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"            "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  #####     "<<endl;
+ cout<<"  #####     "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"            "<<endl;
+ cout<<"  ########  "<<endl;
+ cout<<"       ##   "<<endl;
+ cout<<"      ##    "<<endl;
+ cout<<"     ##     "<<endl;
+ cout<<"    ##      "<<endl;
+ cout<<"   ######## "<<endl;
+ cout<<"            "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  #####     "<<endl;
+ cout<<"  #####     "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"            "<<endl;
+ cout<<"    ###     "<<endl;
+ cout<<"  ##   ##   "<<endl;
+ cout<<"  ##        "<<endl;
+ cout<<"   ####     "<<endl;
+ cout<<"      ###   "<<endl;
+ cout<<"        ##  "<<endl;
+ cout<<"  ##    ##  "<<endl;
+ cout<<"   #####    "<<endl;
+
+}
